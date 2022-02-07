05f68592077cfbe9c46084f3e89b75cec1f20d59
DhruvBhatnagar <dhruv.sandiego@gmail.com>
2022-02-06 19:59:26 -0800

    frq 3 update

--- src/main/java/com/nighthawk/csa/FRQTHREE.java
+++ src/main/java/com/nighthawk/csa/FRQTHREE.java
@@ -2,9 +2,6 @@ package com.nighthawk.csa;
 
 import java.util.Scanner;  // Import the Scanner class
 
-
-import java.util.Scanner;  // Import the Scanner class
-
 class FRQTHREE {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
@@ -14,7 +11,9 @@ class FRQTHREE {
         String attending = myObj.nextLine();  // Read user input
         System.out.println("you are attending " + attending)
         ;  // Output user input
-        System.out.println("What food are you going to eat?  1 represents beef, 2 represents chicken, 3 represents pasta, and all other values represent fish");
+
+        System.out.println("What food are you going to eat?  1 represents beef, 2 represents chicken, 3 represents pasta, and all other values represent");
+
         int food = myObj.nextInt();
         if( food == 1 ){
             System.out.println("Beef");
@@ -26,7 +25,7 @@ class FRQTHREE {
         if(food == 3 ){
             System.out.println("pasta");
         }
-        if(food >= 4){
+        if(food > 4){
             System.out.println("fish");
         }
 
