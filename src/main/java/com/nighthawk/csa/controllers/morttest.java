376dfcee5852e37f3237e8718104667491d00e5b
samuelmahjouri <samuel.mahjouri@gmail.com>
2022-02-15 09:36:11 -0800

    here's my broken test

---
+++ src/main/java/com/nighthawk/csa/controllers/morttest.java
@@ -0,0 +1,42 @@
+package com.nighthawk.csa.starters.squirrel;
+import java.util.ArrayList;
+import java.util.Random
+
+
+// Write a Class Number
+public class NumberOfSquirrels(int a, String b) {
+    // instance variables
+    age = a;
+    breed = b;
+}
+
+            // Number has a zero Argument constructor
+
+            // It initializes a random number between 3 and 36, ie the number of squirrels in class
+
+            // constructor
+
+            // It contains a getter for the Random Number
+
+            randomnumber = this.randomnumber
+
+            // It contains a getter for Index, or the order it was initialized
+
+            this.i = ""
+
+// Write a tester method
+public static void main(String[] args) {
+
+        // Create an ArrayList of Type Number, my ArrayList is called squirrels
+
+        // Initialize 10 squirrels of class type Number
+
+        // Insert Number instance into ArrayList Squirrel in least to greatest order by random number, mine required nested loops
+
+        // Print a formatted message with number of Squirrels and Index by which they were created, use enhanced for loop
+
+        println("Squirrels" + this.number + "Day" + this.day)
+
+        }
+
+        }
